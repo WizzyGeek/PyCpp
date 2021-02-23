73b4bce4931234fe3ce4e5eb199b911fcca98bb4
Vivaan Verma <vivaan.verma@gmail.com>
2021-02-23 15:39:37 +0000

    Added more vector methods

--- PyCpp/vector.py
+++ PyCpp/vector.py
@@ -1,46 +1,59 @@
 
-class Vector():
+class vector():
 
     def __init__(self, data: list) -> None:
-        self.data = data
+        self.data_ = data
+        return
+
+    def __getitem__(self, key: int):
+        return self.data_[key]
+
+    def __setitem__(self, key: int, value) -> None:
+        self.data_[key] = value
         return
 
     def push_back(self, val) -> None:
         "Adds an element to the end of the vector"
-        self.data.append(val)
+        self.data_.append(val)
 
     def pop_back(self) -> None:
         "Removes the last element of the vector"
-        self.data.pop(-1)
+        self.data_.pop(-1)
 
     def at(self, index: int):
         "Gets the element at specified index"
-        return self.data[index]
+        return self.data_[index]
 
     def size(self) -> int:
         "Returns the size of the vector"
-        return len(self.data)
+        return len(self.data_)
 
     def empty(self) -> bool:
         "Test whether vector is empty"
-        return len(self.data) == 0
+        return len(self.data_) == 0
 
     def front(self):
         "Returns the first element of a vector"
-        return self.data[0]
+        return self.data_[0]
 
     def back(self):
         "Returns the last element of a vector"
-        return self.data[-1]
+        return self.data_[-1]
 
-    def data_(self):
+    def data(self):
         "Returns the data the vector holds"
-        return self.data
+        return self.data_
     
     def insert(self, position: int, val) -> None:
-        self.data.insert(position, val)
+        self.data_.insert(position, val)
         return
 
     def clear(self) -> None:
-        self.data = []
+        self.data_ = []
+        return
+
+    def erase(self, position: int, end_position: int = None) -> None:
+        end_position = (end_position if not end_position == None else position) + 1
+        for i in range(position, end_position):
+            self.data_.pop(i)
         return
